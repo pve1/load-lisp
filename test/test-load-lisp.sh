cbb00264f444a5c39731697740682fc8faff7796
pve <peterve@gmail.com>
2012-02-16 01:50:29 +0200

    Added test.

--- test/test-load-lisp.sh
+++ test/test-load-lisp.sh
@@ -7,16 +7,18 @@ cd $HERE
 
 function test-ll () {
     touch test-system-1.lisp
-    rm test-1.output test-1.error test-2.output test-2.error all-tests.output
+    rm *.out *.err all-tests.output
     
-    $LOADLISP --disable-debugger -T . -R load-lisp.test-system-1 --quit > test-1.output 2> test-1.error
-    $LOADLISP --disable-debugger -T . -R load-lisp.test-system-1 ---main-1 ---main-2 --quit > test-1.output 2>> test-1.error
-    cat test-1.output >> all-tests.output
+    $LOADLISP --disable-debugger -T . -R load-lisp.test-system-1 --quit > test-1.out 2> test-1.err
+    $LOADLISP --disable-debugger -T . -R load-lisp.test-system-1 ---main-1 ---main-2 --quit > test-1.out 2>> test-1.err
+    cat test-1.out >> all-tests.output
 
-    $LOADLISP --disable-debugger -T . -R load-lisp.test-system-1 --save-exe test.core ---main-1 ---main-2 --quit > test-2.output 2>> test-2.error
-    ./test.core > test-2.output 2>> test-2.error
-    cat test-2.output >> all-tests.output
+    $LOADLISP --disable-debugger -T . -R load-lisp.test-system-1 --save-exe "a test.core" ---main-1 ---main-2 --quit > test-2.out 2>> test-2.err
+    "./a test.core" > test-2.out 2>> test-2.err
+    cat test-2.out >> all-tests.output
 
+    $LOADLISP --core "a test.core" --disable-debugger ---main-1 ---main-2 --quit  > test-3.out 2>> test-3.er
+    cat test-3.out >> all-tests.output
     diff -U 10 all-tests.output expected.output
 }
 
