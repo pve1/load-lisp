c27282a4312864ff685ea3dd13cbb0ffeedb6d06
pve <peterve@gmail.com>
2012-02-17 20:22:37 +0200

    Made handle-args script use arg-handler-installed-p.

--- handle-args.lisp
+++ handle-args.lisp
@@ -3,4 +3,7 @@
 (in-package :cl-user)
 (require :asdf)
 (load (merge-pathnames "system-loader" *load-truename*))
-(load-lisp:handle-posix-argv)
+
+;; Avoid recursive calls
+(unless (load-lisp:arg-handler-installed-p)
+  (load-lisp:handle-posix-argv))
